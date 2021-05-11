af5fb79c774f008a43f9561047fb10cad96b8ee6
Valentin Dimitroff <26602880+ValentinnDimitroff@users.noreply.github.com>
2021-05-11 12:01:06 +0300

    Finish Group show styling

--- src/components/groups/GroupShow.jsx
+++ src/components/groups/GroupShow.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import { Show, ShowTitle, GridShowLayout, Datagrid, DeleteButton, FunctionField, TextField, RaGrid, ReferenceField, ReferenceManyField } from '../_design'
 import { resourcesMap } from '../../constants'
-// import { makeStyles } from '@material-ui/styles'
 import { Pagination } from 'ra-ui-materialui'
 import AddUserButton from './buttons/AddUserButton'
+import { makeStyles } from '@material-ui/styles'
 
-// const useStyles = makeStyles(() => ({
-//     usersLabel: {
-//         display: 'flex',
-//         justifyContent: 'space-between',
-//         alignItems: 'center'
-//     }
-// }))
+const useStyles = makeStyles(() => ({
+    usersGrid: {
+        "& > div": {
+            display: 'flex',
+            flex: "0 0 100%",
+        }
+    }
+}))
 
 const GroupShow = props => {
+    const classes = useStyles()
 
     return (
         <Show {...props} title={<ShowTitle render={r => `Group - ${r.title}`} />}>
@@ -30,12 +32,13 @@ const GroupShow = props => {
                             target={resourcesMap.groups.refId}
                             perPage={10}
                             pagination={<Pagination />}
+                            className={classes.usersGrid}
                         >
                             <Datagrid>
                                 <ReferenceField link="show" label="Full name" source={resourcesMap.users.refId} reference={resourcesMap.users.name}>
                                     <FunctionField render={r => `${r.firstName} ${r.lastName}`} />
                                 </ReferenceField>
-                                <DeleteButton redirect={false}/>
+                                <DeleteButton redirect={false} />
                             </Datagrid>
                         </ReferenceManyField>
                         <AddUserButton fullWidth />
---
+++ src/ra-providers/index.js
@@ -0,0 +1,2 @@
+export { default as dataProvider } from './dataProvider'
+export { default as i18nProvider } from './i18nProvider'
